4bd75fdf43b1660e6edee31e2d71f48b2ee2846a
Sidharth Subramanian <sidharth.subramanian.97@gmail.com>
2022-03-30 11:24:00 +0530

    Added basic testing scenario

---
+++ react/ci-playground/src/hello/hello.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable testing-library/no-unnecessary-act */
+// hello.test.js
+
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "@testing-library/react";
+
+import Hello from "./hello";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hey, stranger");
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Margaret!");
+});
